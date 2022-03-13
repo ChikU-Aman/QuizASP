31bdd330d7f0980563bfdd70bae2e2a2d6b86044
aman.singh <aman.singh@YI1012926LT>
2022-03-13 21:29:11 +0530

    Updated Local

--- QUIZ/Models/QUIZContext.cs
+++ QUIZ/Models/QUIZContext.cs
@@ -25,7 +25,7 @@ namespace QUIZ.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=QuizSKP.mssql.somee.com;Initial Catalog=QuizSKP;Integrated Security=False;Persist Security Info=False;User ID=asraj23_SQLLogin_1;Password=hqjjannu4f");
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=QUIZ;Trusted_Connection=True;MultipleActiveResultSets=true");
             }
         }
 
